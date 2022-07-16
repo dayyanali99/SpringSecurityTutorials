47e187fe2e3468b127d180057d9db529c046460c
dayyanali <dayyanali99@gmail.com>
2022-07-16 13:21:02 +0500

    First Commit.

---
+++ src/main/java/com/springboot/springsecuritytutorials/student/Student.java
@@ -0,0 +1,5 @@
+package com.springboot.springsecuritytutorials.student;
+
+public record Student(Integer studentId, String studentName)
+{
+}
---
+++ src/test/java/com/springboot/springsecuritytutorials/SpringSecurityTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecuritytutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
